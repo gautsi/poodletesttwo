d1057c6569cf948182acd4ec6467b064f6012f27
Gautam <gautam.sisodia@gmail.com>
2015-07-26 18:12:19 -0400

    First commit

---
+++ README.rst
@@ -0,0 +1,12 @@
+===============================
+Poodle test two
+===============================
+
+.. image:: https://img.shields.io/travis/gautsi/poodletesttwo.svg
+        :target: https://travis-ci.org/gautsi/poodletesttwo
+
+My second tes
+
+* Free software: BSD license
+* Documentation: https://poodletesttwo.readthedocs.org.
+
---
+++ tests/test_poodletesttwo.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_poodletesttwo
+----------------------------------
+
+Tests for `poodletesttwo` module.
+"""
+
+import unittest
+
+from poodletesttwo import poodletesttwo
+
+
+class TestPoodletesttwo(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
