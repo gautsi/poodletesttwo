7e6434addf2095ed9e00a5d13a829717cadb32f5
Gautam <gautam.sisodia@gmail.com>
2015-07-26 19:01:00 -0400

    a test of the travis ci service

--- poodletesttwo/poodletesttwo.py
+++ poodletesttwo/poodletesttwo.py
@@ -3,7 +3,8 @@
 
 def test_func():
     """
-    A test function for doctests and unitests with tox.
+    A test function for doctests and unitests with tox. This is
+    second testing and stuff.
     For example,
 
     >>> test_func()
