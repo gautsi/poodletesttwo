c8abfb8b897e465e58946a9b9ef24f46b7481b20
Gautam <gautam.sisodia@gmail.com>
2015-07-27 15:35:18 -0400

    further testing of dependencies

--- poodletesttwo/poodletesttwo.py
+++ poodletesttwo/poodletesttwo.py
@@ -1,15 +1,21 @@
 # -*- coding: utf-8 -*-
+"""A test package for things."""
 import numpy as np
 import pandas as pd
+import praw as pr
+import time as tm
+from sqlalchemy.exc import StatementError
 
 
 def test_func():
     """
-    A test function for doctests and unitests with tox. This is
-    second testing and stuff.
+    A test function for doctests and unitests with tox.
+
+    This is second testing and stuff.
     For example,
 
-    >>> test_func()
+    >>> a = test_func()
+    >>> a
     'hello 2.0 0'
 
     >>> 1 + 2
@@ -20,4 +26,8 @@ def test_func():
 
     """
     df = pd.DataFrame([0])
+    red = pr.Reddit(user_agent='creddit_score')
+    del red
+    a = tm.time()
+    del a
     return "hello {} {}".format(np.sqrt(4), df[0][0])
